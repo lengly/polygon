fd52a53ae70cd903e41fa8b7238026de132ef740
lengly <liu_yuda@163.com>
2015-12-08 17:08:21 +0800

    finish draw polygon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # coding: utf8
 import Tkinter
 from Tkinter import *
-import polygon as P
+from polygon import *
 
 top = Tk()
 top.geometry('500x500')
@@ -10,18 +10,21 @@ C = Tkinter.Canvas(top, bg="gray", height=500, width=500)
 C.pack(side=LEFT)
 
 point = []
-
+p = None
 
 # 鼠标点击画线
 def record_coordinate(event):
+    global p
     print event.x,event.y
     if len(point) > 0:
         if abs(event.x - point[0].x) + abs(event.y - point[0].y) < 10:
             C.create_line(point[0].x, point[0].y, point[-1].x, point[-1].y, fill='red')
+            p = Polygon(point)
+            p.paint(C)
             return
         else:
             C.create_line(point[-1].x, point[-1].y, event.x, event.y, fill='red')
-    point.append(P.Point(event.x, event.y))
+    point.append(Point(event.x, event.y))
 
 C.bind("<Button 1>", record_coordinate)
 
--- polygon.py
+++ polygon.py
@@ -8,9 +8,59 @@ class Point:
 
 
 class Line:
-    # 保证第一个点y值不低于第二个点
-    def __init__(self, point_1, point_2):
-        self.point_1 = point_1
-        self.point_2 = point_2
-        if self.point_1.y > self.point_2.y:
-            self.point_1, self.point_2 = self.point_2, self.point_1
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        if p1.y == p2.y:
+            self.delta = 0
+        else:
+            self.delta = float(p1.x - p2.x) / (p1.y - p2.y)
+        self.x = p1.x if p1.y < p2.y else p2.x
+        self.yMax = max(p1.y, p2.y)
+        self.yMin = min(p1.y, p2.y)
+
+
+class Polygon:
+    # 构造出线  并记录下多边形y坐标轴的区间范围
+    def __init__(self, point):
+        self.line = []
+        self.yMin = point[-1].y
+        self.yMax = point[-1].y
+        for i in range(len(point) - 1):
+            self.line.append(Line(point[i], point[i+1]))
+            self.yMin = min(self.yMin, point[i].y)
+            self.yMax = max(self.yMax, point[i].y)
+        if len(point) > 2:
+            self.line.append(Line(point[0], point[-1]))
+
+    # 填充多边形颜色
+    def paint(self, canvas):
+        net = {}
+        # 初始化新边表头指针net
+        for line in self.line:
+            if line.yMin not in net:
+                net[line.yMin] = []
+            net[line.yMin].append(line)
+        aet = []
+        print self.yMin, self.yMax
+        for y in range(self.yMin, self.yMax + 1):
+            # 把新边表net[line]中的边节点加入aet表中  使之按x坐标递增排序
+            if y in net:
+                aet.extend(net[y])
+            aet.sort(lambda a,b: cmp(a.x, b.x))
+            i = 0
+            if y in net:
+                while i < len(aet) - 1:
+                    if abs(aet[i].x - aet[i+1].x) < 1e-5:
+                        if aet[i+1].yMin < y:
+                            aet.pop(i+1)
+                        if aet[i].yMin < y:
+                            aet.pop(i)
+                    i += 1
+            for i in range(0, len(aet) - 1, 2):
+                canvas.create_line(aet[i].x, y, aet[i+1].x, y, fill='red')
+            aet = [w for w in aet if w.yMax > y]
+            for w in aet:
+                w.x += w.delta
+
+
